a63c66cf88086d8ac1a87eafa08cb5ef6f84b587
madibag <62722024+madibag@users.noreply.github.com>
2020-10-17 00:04:51 +0300

    Update upnload.py

--- upnload.py
+++ upnload.py
@@ -1,8 +1,9 @@
-
+import os
 
 def nload(u,c,name,capt):
-	chat_id = u.message.chat.id
-	context.bot.send_video(chat_id = chat_id,
-								video= open(name, 'rb'),
-								caption = capt
-								)
+    chat_id = u.message.chat.id
+    context.bot.send_video(chat_id = chat_id,
+                                video= open(name, 'rb'),
+                                caption = capt
+                                )
+    os.remove(name)
