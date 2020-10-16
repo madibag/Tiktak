182511846a8a753d7e69143bb101a24a76572368
madibag <62722024+madibag@users.noreply.github.com>
2020-10-17 00:32:34 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,9 +12,8 @@ else:
 def start(update,context):
     name = update.message.chat.first_name
     chat_id = update.message.chat_id
-    context.bot.send_message(chat_id = chat_id,
-        text="Hello {} Welcome to My Tiktok\nVideo Downloading Bot For more Help\nSent /\help \n<a href='https://t.me/nahom_d'>developer<a>".format(name),
-        parse_mode = ParseMode.HTML, disable_web_page_preview=True
+    update.message.reply_text(text="Hello {} Welcome to My Tiktok\nVideo Downloading Bot For more Help\nSent /\help \n<a href='https://t.me/nahom_d'>developer<a>".format(name),
+                              parse_mode = ParseMode.HTML, disable_web_page_preview=True
                             )
 def help(update,context):
     update.message.reply_text('To download videos\nsimply paste you download link\nThen send')
